883d39d0111a6486b8222b9ccee00db99b380e9d
Winfield H <winterfreshness@gmail.com>
2018-12-14 20:29:19 -0500

    Created the readme and added images.

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # bamazon
 An Amazon-like storefront CLI app written using mySQL and node.js. The app will take in orders from customers and deplete stock from the store's inventory.
+
+## 1. Displaying the products
+In the beginning, a table is shown with the following properties:
+*Item ID
+*Product Name
+*Price
+*Stock Quantity
+
+![product table](images/bamazon-intro.JPG)
+
+## 2. Asking for the item and quantity
+The app prompts the user for the item ID of the product he or she wants as well as the quantity of that item.
+
+![questions](images/bamazon-question.JPG)
+
+## 3. Input validation
+The app also validates the input, so the input should be an acceptable item ID and the quantity is a number.
+
+![input validation](images/bamazon-validation.gif)
+
+## 4. Stock update
+Once an acceptable ID and quantity are entered, the app checks the inventory and attempts to update the database. If the buying quantity is less than the number of stock, it'll deduct the amount and display the total cost to the user. If the buying amount exceeds the stock, an error is displayed and will have to try the program again.
+
+![stock update](images/bamazon-stock-update.JPG)
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+let productArray;
 let id;
 
 // make a variable that has the database connection info
@@ -19,7 +20,7 @@ function start() {
     connection.query(query, (err, res) => {
         // catch the error or show the data
         (err) ? console.log(err): console.table(res)
-        connection.end();
+        productArray = res;
         let limit = res.length;
 
         // ask the user for a choice
@@ -44,7 +45,6 @@ function start() {
                 // verify the answer is valid, else restart
                 if (answers.itemID > 0 && answers.itemID <= limit) {
                     id = answers.itemID;
-                    console.log(`Your choice: ${id}`);
                     
                     // ask how many units the user wants to buy
                     howMany();
@@ -72,14 +72,29 @@ function howMany() {
             }
         })
         .then(answers => {
-            console.log(`You want to buy: ${answers.numUnits} units`);
             // attempt to fulfill the order
-            fulfillOrder(answers.numUnits);
+            fulfillOrder(productArray[id - 1], answers.numUnits);
         });
 }
 
-function fulfillOrder(units) {
-
+function fulfillOrder(product, units) {
+    // compare the amounts
+    if (units <= product.stock_quantity) {
+        let newStock = product.stock_quantity - units;
+        let totalCost = product.price * units;
+        // console.log(newStock);
+        let query = "UPDATE products SET stock_quantity = ? WHERE item_id= ?";
+        connection.query(query, [newStock, id], (err, res) => {
+            (err) ? console.log(err): console.log(`.\n.\n.\nStock updated!\nYour total is: $${totalCost}`)
+            // connection.end();
+            start();
+        });
+        
+    } else {
+        console.log(`Insufficient quantity!`);
+        // connection.end();
+        start();
+    }
 }
 
 // MAIN CODE =====================================================================
